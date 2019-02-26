ea35f73d09a263c9184fa12b4a12ac41d3018e6d
djh <>
2019-02-26 17:31:47 +0800

    提交

--- src/SharePlatformSystem.Test/NHibernate/Entitys/TestEntity.cs
+++ src/SharePlatformSystem.Test/NHibernate/Entitys/TestEntity.cs
@@ -6,8 +6,13 @@ using System.Text;
 
 namespace SharePlatformSystem.Test.NHibernate.Entitys
 {
-    public class TestEntity: FullAuditedEntity<string>
+    public class TestEntity: FullAuditedEntity<string>, IHasCreationTime
     {
         public virtual string TestName { get; set; }
+        public virtual DateTime CreationDate { get; set; }
+
+        public virtual DateTime? ModificationDate { get; set; }
+        public virtual ICollection<TestDetail> Items { get; set; }
+
     }
 }
